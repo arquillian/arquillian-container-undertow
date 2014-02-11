27f2113a98c8eb44f03605c084a15c96926ced8e
alex <asotobu@gmail.com>
2014-02-11 23:54:54 +0100

    resolves issue #1 by adding random port election.

--- undertow-embedded/src/main/java/org/arquillian/undertow/UndertowContainerConfiguration.java
+++ undertow-embedded/src/main/java/org/arquillian/undertow/UndertowContainerConfiguration.java
@@ -1,16 +1,24 @@
 package org.arquillian.undertow;
 
+import java.util.Random;
+
 import org.jboss.arquillian.container.spi.ConfigurationException;
 import org.jboss.arquillian.container.spi.client.container.ContainerConfiguration;
 
 public class UndertowContainerConfiguration implements ContainerConfiguration {
+
+	private static final int RANDOM_PORT_FLAG = -1;
+	private static final int MAX_PORT = 48128;
+	private static final int MIN_PORT = 1024;
 
 	private String bindAddress = "localhost";
 
 	private int bindHttpPort = 8080;
 
 	public void validate() throws ConfigurationException {
-
+		if(bindHttpPort == RANDOM_PORT_FLAG) {
+			bindHttpPort = generateRandomPort();
+		}
 	}
 
 	public String getBindAddress() {
@@ -29,4 +37,10 @@ public class UndertowContainerConfiguration implements ContainerConfiguration {
 		this.bindHttpPort = bindHttpPort;
 	}
 
+	private int generateRandomPort() {
+		Random random = new Random();
+		int nextPortZeroBased = random.nextInt(MAX_PORT);
+		return nextPortZeroBased + MIN_PORT;
+	}
+	
 }
